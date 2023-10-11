07da5e97f21a51f90ad5d56050cea27ac1857f2e
sthota3 <sreehari.thota@slu.edu>
2023-10-11 14:40:50 -0500

    Added Login and SignUp componennts

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import Header from './components/Header'; // Import your Header component
 import Footer from './components/Footer'; // Import your Footer component
+import Component from './components/Component';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Header /> {/* Include your Header component */}
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Component />} />
         </Routes>
         <Footer /> {/* Include your Footer component */}
       </div>
---
+++ my-react/src/components/Component.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import "../CSS/Component.css";
+
+const userObj = {
+  name: '',
+  contactId: '',
+  email: '',
+  address: '',
+  zipCode: '',
+  userName: '',
+  password: '',
+  confirmPassword: '',
+  isTNC: false
+}
+
+function Signupandsignin() {
+  const [signIn, toggleSignIn] = useState(true);
+  const [user, setUser] = useState(userObj);
+  
+
+  const toggleForm = () => {
+    toggleSignIn(!signIn);
+  };
+
+  const handleChange = (e)=>{
+    setUser({...user,...{[e.target.id]:e.target.value}});
+  }
+  
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const _userObj = {   
+      "Name" : user.name,
+      "ContactId" : user.contactId, 
+      "Email" : user.email,
+      "Address" : user.address ,
+      "ZipCode" : user.zipCode,
+      "UserName" : user.userName,
+      "Password" : user.password
+    }
+    axios.post(API_URL+"/register",_userObj).then(res=>{
+      if(res && res.data && res.data.issuccess){
+        window.alert("DONE");
+      }
+      else{
+        window.alert("FAILURE");
+      }
+    }).catch(err=>{
+      console.log(err);
+      window.alert("FAILURE");
+    })
+  }
+  return (
+    <div className="container">
+      <div className={`signup-container ${signIn ? "signin" : ""}`}>
+        <form>
+          <h1>REGISTER</h1>
+          <input type="text" placeholder="Name" id='name' value={user.name} onChange={handleChange} />
+          <input type="text" placeholder="Phone Number" id='contactId' value={user.contactId} onChange={handleChange} />
+          <input type="email" placeholder="Email" id='email' value={user.email} onChange={handleChange} />
+          <input type="text" placeholder="Address" id='address' value={user.address} onChange={handleChange} />
+          <input type="text" placeholder="Zip Code" id='zipCode' value={user.zipCode} onChange={handleChange} />
+          <input type="text" placeholder="UserName" id='userName' value={user.userName} onChange={handleChange} />
+          <input type="password" placeholder="Password" id='password' value={user.password} onChange={handleChange} />
+          <input type="password" placeholder="Re-enter Password" id='confirmPassword' value={user.confirmPassword} onChange={handleChange}  />
+          <label className="terms-label"><input type="checkbox" /><span>I agree to the Terms and Conditions</span></label>
+          <button onClick={handleSubmit}>Sign Up</button>
+
+        </form>
+      </div>
+
+      <div className={`signin-container ${signIn ? "" : "signin"}`}>
+        <form>
+          <h1>SIGN-IN</h1>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forgot your password?</a>
+          <button>Sign In</button>
+        </form>
+      </div>
+
+      <div className={`overlay-container ${signIn ? "signin" : ""}`}>
+        <div className={`overlay ${signIn ? "signin" : ""}`}>
+          <div className="overlay-panel left-overlay-panel">
+            <h1>Welcome Back!</h1>
+            <p>To keep connected with us, please login with your personal info</p>
+            <p>Don't have an Account</p>
+            <button className="ghost-button" onClick={toggleForm}>
+              Sign up
+            </button>
+          </div>
+
+          <div className="overlay-panel right-overlay-panel">
+            <h1>Hello, Friend!</h1>
+            <p>Enter your personal details and start your journey with us</p>
+            <p>Already have an Account</p>
+            <button className="ghost-button" onClick={toggleForm}>
+              Sign In
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signupandsignin;
