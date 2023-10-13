a8fd7a639313f09bf42fa91b854272a2ee67a3ac
deepthiannem <pannem1>
2023-10-13 14:57:01 -0500

    MEC-11 : Added styling to the homepage and Api.js file for API URL

--- my-react/src/Services/Api.js
+++ my-react/src/Services/Api.js
@@ -1,1 +1,4 @@
 export const API_URL = "C:\Users\mukti\OneDrive\Desktop\PSD_MECHAZONE\MechaZone\my-react\src\Services\Api.py";
+
+// export const API_URL = "C:\Users\deept\Documents\Mechazone\MechaZone\my-react\src\Services\Api.py";
+
