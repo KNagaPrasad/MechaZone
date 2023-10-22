1cef64c91ee4e4d6b2786ace9c0b3ca1956ca8fd
deepthiannem <pannem1>
2023-10-22 13:21:00 -0500

    Added cart option to our Mechazone

--- my-react/src/components/Header.js
+++ my-react/src/components/Header.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link
-import Logo from './Logo'; // Import the Logo component
+import { Link } from 'react-router-dom';
+import Logo from './Logo';
 import '../CSS/Header.css';
 import { useSelector } from 'react-redux';
-import NavDropDown from './NavDropDown'
+import { FaShoppingCart } from 'react-icons/fa'; // Import the cart icon
+import NavDropDown from './NavDropDown';
 
 function Header() {
   const userFromRedux = useSelector(state => state?.userInfo?.user);
-  console.log(userFromRedux);
+
   return (
     <header className="header">
       <div className="dashboard">
-        <Logo /> 
-        <NavDropDown user = {userFromRedux} />
+        <Logo />
+        <NavDropDown user={userFromRedux} />
       </div>
       <nav className="navigation">
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          {!userFromRedux && (
+            <>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </>
+          )}
         </ul>
       </nav>
+      {userFromRedux && (
+        <div className="cart-icon">
+          <Link to="/cart">
+            {/* Set the color prop to white */}
+            <FaShoppingCart size={30} color="white" />
+          </Link>
+        </div>
+      )}
     </header>
   );
 }
