6135ce8b0c013dc92caf8e28c6b9b29f416d5346
Vmuktineni <vineetha.muktineni@slu.edu>
2023-11-10 15:39:49 -0600

    Implemented backend for displaying total price of the items in the cart and displaying them in frontend

--- my-react/src/Services/test_app.py
+++ my-react/src/Services/test_app.py
@@ -44,3 +44,71 @@ def test_get_bikes():
         response = client.get('/bike')
         assert response.status_code == 200   
 
+def test_get_car_spares():
+    with app.test_client() as client:
+        response = client.post('/getCarSpares', json={'carId': 1})
+        assert response.status_code == 200
+
+def test_add_to_cart_items():
+    with app.test_client() as client:
+        cart_item_data = {
+            "user_id": 1,
+            "s_id": 1
+        }
+        response = client.post('/addToCart', json=cart_item_data)
+        assert response.status_code == 200
+        assert response.json['issuccess'] is True 
+
+def test_display_total():
+    with app.test_client() as client:
+        cart_data = {
+            "user_id": 1
+        }
+        response = client.post('/prepareShoppingCart', json=cart_data)
+        assert response.status_code == 200
+        assert 'shoppingCart' in response.json 
+
+# def test_get_spare_part_amount():
+#     with app.test_client() as client:
+#         spare_ids = [1, 2, 3]  # Replace with valid spare part IDs
+#         response = client.post('/getSparePartAmount', json={'s_ids': spare_ids})
+#         assert response.status_code == 200
+#         assert 'totalPrice' in response.json
+#         assert 'spareParts' in response.json  
+# 
+                    
+# def test_display_discount():
+#     with app.test_client() as client:
+#         cart_data = {
+#             "user_id": 1
+#         }
+#         response = client.post('/prepareShoppingCart', json=cart_data)
+#         assert response.status_code == 200
+#         assert 'shoppingCart' in response.json
+#         assert 'discount' in response.json
+
+# def test_display_delivery_type():
+#     with app.test_client() as client:
+#         cart_data = {
+#             "user_id": 1
+#         }
+#         response = client.post('/prepareShoppingCart', json=cart_data)
+#         assert response.status_code == 200
+#         assert 'shoppingCart' in response.json
+#         assert 'delivery_type' in response.json
+
+def test_choose_delivery_type():
+    with app.test_client() as client:
+        delivery_data = {
+            "cart_id": 1,
+            "user_id": 1,
+            "delivery_type": "Home"  # Replace with a valid delivery type
+        }
+        response = client.post('/deliveryType', json=delivery_data)
+        assert response.status_code == 200
+        assert response.json['issuccess'] is True
+
+def test_display_car_spares():
+    with app.test_client() as client:
+        response = client.post('/getCarSpares', json={'carId': 1})
+        assert response.status_code == 200        
