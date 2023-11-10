c1635b06bacfdcc3b7d9ac66b050cd08d389977f
KNagaPrasad <nagaprasad.kondaboina@slu.edu>
2023-11-10 17:31:13 -0600

    MEC38 added the checkout css and deliverypage

--- my-react/src/components/CheckOutPage.js
+++ my-react/src/components/CheckOutPage.js
@@ -1,8 +1,64 @@
 import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import '../CSS/CheckoutPage.css';
 
 const CheckoutPage = () => {
   const location = useLocation();
+  const productDetails = location.state?.productDetails || [];
+
+  // Calculate total price, discount, and amount
+  const totalPrice = productDetails.reduce((total, product) => total + product.price, 0);
+  const discount = totalPrice * 0.1; // Assuming a 10% discount
+  const amount = totalPrice - discount;
+
+  const [formData, setFormData] = useState({
+    deliveryAddress: '',
+    paymentMethod: '',
+  });
+
+  const [deliveryType, setDeliveryType] = useState('');
+  const [selectedStore, setSelectedStore] = useState('');
+  const [otherAddress, setOtherAddress] = useState({
+    street: '',
+    apt: '',
+    zipcode: '',
+    city: '',
+    phoneNumber: '',
+  });
+
+  const [storeOptions, setStoreOptions] = useState(['Store1', 'Store2', 'Store3']); // Replace with your actual store names
+  const [searchedStores, setSearchedStores] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleDeliveryTypeChange = (selectedType) => {
+    setDeliveryType(selectedType);
+    if (selectedType === 'Store') {
+      // Placeholder for store data
+      // You can fetch and set actual store data here
+      setSearchedStores(storeOptions);
+    }
+  };
+
+  const handleOtherAddressChange = (e) => {
+    const { name, value } = e.target;
+    setOtherAddress({ ...otherAddress, [name]: value });
+  };
+
+  const handleStoreSelect = (selectedStore) => {
+    setSelectedStore(selectedStore);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add logic for handling form submission (e.g., sending data to server)
+    console.log('Form Submitted:', { formData, deliveryType, selectedStore, otherAddress });
+  };
 
   return (
     <div className="checkout-container">
@@ -20,10 +76,51 @@ const CheckoutPage = () => {
           <button type="button" className={deliveryType === 'Store' ? 'selected' : ''}>
             Store
           </button>
+          {deliveryType === 'Store' && (
+            <div className="store-dropdown">
+              <label>Select the Store:</label>
+              <select
+                value={selectedStore}
+                onChange={(e) => setSelectedStore(e.target.value)}
+              >
+                <option value="" disabled>Select a Store</option>
+                {storeOptions.map((store) => (
+                  <option key={store} value={store}>
+                    {store}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
         </div>
+        <div className="delivery-option" onClick={() => handleDeliveryTypeChange('Other')}>
+          <button type="button" className={deliveryType === 'Other' ? 'selected' : ''}>
+            Other
+          </button>
+        </div>
+
+      <div className="sub-container product-details">
+        <h3>Product Details</h3>
+        {productDetails.map((product) => (
+          <div key={product.id}>
+            <p>{product.name}</p>
+            <p>{product.description}</p>
+            <p>Price: ${product.price}</p>
+          </div>
+        ))}
+        <div className="price-summary">
+          <p>Tires      : $100</p>
+          <p>Spark Plugs: $10</p>
+          <p>Total Price: $110</p>
+          <p>Discount   : $11</p>
+          <p>Amount     : $99</p>
+        </div>
+          <button type="button" onClick={()=> navigate('/dashboard')}>CONTINUE SHOPPING</button>
       </div>
+
+      {/* Payment Section */}
     </div>
   );
 };
 
-export default CheckoutPage;
+export default CheckoutPage;
