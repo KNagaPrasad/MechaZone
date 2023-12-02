ddd739ae4411e03fddacf920e505d3e8048c7f99
deepthiannem <pannem1>
2023-12-02 17:38:44 -0600

    Added the exit button , so that user can exit once the transcation is done

--- my-react/src/components/SuccessPage.js
+++ my-react/src/components/SuccessPage.js
@@ -1,71 +1,5 @@
-// import React, { useEffect, useState } from 'react';
-
-// const SuccessPage = () => {
-//   const [loading, setLoading] = useState(true);
-//   const [transactionNumber, setTransactionNumber] = useState('');
-
-//   useEffect(() => {
-//     const generateTransactionNumber = () => {
-//       // For demonstration, you can replace this with your actual logic for generating a transaction number
-//       const randomTransactionNumber = Math.floor(Math.random() * 1000000);
-//       setTransactionNumber(randomTransactionNumber);
-//     };
-
-//     // Simulate a delay of 3 seconds before showing the success message
-//     const delay = setTimeout(() => {
-//       setLoading(false);
-//       generateTransactionNumber();
-//     }, 1000);
-
-//     return () => clearTimeout(delay); // Clear the timeout if the component unmounts
-
-//   }, []); // Empty dependency array to run the effect only once
-
-//   const styles = {
-//     paymentsContainer: {
-//       maxWidth: '800px',  // Adjusted width
-//       margin: '0 auto',
-//       padding: '20px',
-//       border: '1px solid #ccc',
-//       borderRadius: '5px',
-//       boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-//       backgroundColor: '#fff',
-//       textAlign: 'center',
-//     },
-//     tickIcon: {
-//       fontSize: '100px',  // Increased font size
-//       color: 'green',
-//       marginBottom: '16px',
-//     },
-//     successMessage: {
-//       marginTop: '16px',
-//       color: 'green',
-//       fontWeight: 'bold',
-//       fontSize: '36px',  // Increased font size
-//     },
-//     transactionNumber: {
-//       marginTop: '16px',
-//       fontSize: '24px',  // Increased font size
-//     },
-//   };
-
-//   return (
-//     <div style={styles.paymentsContainer}>
-//       {loading ? (
-//         <div>Loading...</div>
-//       ) : (
-//         <>
-//           <div style={styles.tickIcon}>&#10004;</div>
-//           <h2 style={styles.successMessage}>Transaction Successful</h2>
-//           <div style={styles.transactionNumber}>Transaction Number: {transactionNumber}</div>
-//         </>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default SuccessPage;
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const SuccessPage = () => {
   const [loading, setLoading] = useState(true);
@@ -90,9 +24,10 @@ const SuccessPage = () => {
 
   const styles = {
     paymentsContainer: {
+      position: 'relative',
       maxWidth: '800px',
       margin: '0 auto',
-      padding: '20px',
+      padding: '40px', // Increased padding
       borderRadius: '10px',
       boxShadow: '0 0 20px rgba(0, 128, 0, 0.3)', // Green shadow
       backgroundColor: '#e0f7fa', // Light blue background
@@ -114,6 +49,18 @@ const SuccessPage = () => {
       fontSize: '24px',
       color: '#00695c', // Dark teal
     },
+    exitButton: {
+      position: 'absolute',
+      top: '10px',
+      right: '10px',
+      backgroundColor: '#4CAF50',
+      color: 'white',
+      padding: '10px',
+      borderRadius: '5px',
+      textDecoration: 'none',
+      fontSize: '14px',
+      cursor: 'pointer',
+    },
   };
 
   return (
@@ -125,6 +72,9 @@ const SuccessPage = () => {
           <div style={styles.tickIcon}>&#10004;</div>
           <h2 style={styles.successMessage}>Transaction Successful</h2>
           <div style={styles.transactionNumber}>Transaction Number: {transactionNumber}</div>
+          <Link to="/dashboard" style={styles.exitButton}>
+            Exit
+          </Link>
         </>
       )}
     </div>
@@ -132,4 +82,3 @@ const SuccessPage = () => {
 };
 
 export default SuccessPage;
-
