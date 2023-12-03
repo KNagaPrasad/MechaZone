976d113dc97d9c7c8d143662bbd6dcaf1163a7e6
deepthiannem <pannem1>
2023-12-02 19:36:02 -0600

    Updated the Aboutus page

--- my-react/src/components/AboutUs.js
+++ my-react/src/components/AboutUs.js
@@ -3,10 +3,15 @@ import '../CSS/AboutUs.css';
 
 function AboutUs() {
   return (
-    <div className="about-container">
+    <div className="about-us">
       <h2>About Us</h2>
       <p>
-        Hi , Welcome to mechazone, We made your serach for parts easy!
+      MECHAZONE is a pioneering e-commerce platform dedicated to transforming the automotive experience. Specifically tailored for vehicle enthusiasts and owners, MECHAZONE simplifies the process of purchasing and installing vehicle parts. Our user-friendly interface ensures effortless browsing and shopping, allowing customers to find compatible parts for their vehicles with ease. With flexible delivery options, users can choose between convenient home delivery or opt for professional installation at a nearby repair shop. The seamless integration with local repair shops enhances the overall experience, providing users with skilled professionals for installations. MECHAZONE also offers robust profile management, enabling users to track orders, access purchase history, and stay organized. We are committed to revolutionizing the automotive journey, providing a comprehensive solution for both DIY enthusiasts and those seeking professional services. MECHAZONE is your ultimate destination for automotive excellence.
+
+
+
+
+
         <br />HAPPY TO SERVE YOU!
       </p>
     </div>
