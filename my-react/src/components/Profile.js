08d062594dc7a7bb351c84a7815b97b7153adb51
KNagaPrasad <nagaprasad.kondaboina@slu.edu>
2023-12-08 21:03:20 -0600

    added file

--- my-react/src/components/Profile.js
+++ my-react/src/components/Profile.js
@@ -1,39 +1,28 @@
-import React, { useState } from 'react';  // Ensure that you import React only once
+import React from 'react';
 import '../CSS/Profile.css';
 
 const Profile = () => {
-  const [isProfileOpen, setProfileOpen] = useState(false);
-
-  const toggleProfile = () => {
-    setProfileOpen(!isProfileOpen);
-  };
-
-  // Hardcoded user data for demonstration
+  
   const userData = {
-    name: 'John Doe',
-    contact: '123-456-7890',
-    address: '123 Main St, Cityville',
+    username: 'Mech John',
+    contact: '6368495758',
+    address: '123, lineddll Blvd',
   };
 
   return (
-    <div className={`profile ${isProfileOpen ? 'profile-open' : ''}`}>
-      <button className="profile-button" onClick={toggleProfile}>
-        Profile
-      </button>
-      {isProfileOpen && (
-        <div className="profile-content">
-          <h2>User Profile</h2>
-          <p>
-            <strong>Name:</strong> {userData.name}
-          </p>
-          <p>
-            <strong>Contact:</strong> {userData.contact}
-          </p>
-          <p>
-            <strong>Address:</strong> {userData.address}
-          </p>
-        </div>
-      )}
+    <div className="profile">
+      <div className="profile-content">
+        <h2>User Profile</h2>
+        <p>
+          <strong>Username:</strong> {userData.username}
+        </p>
+        <p>
+          <strong>Contact:</strong> {userData.contact}
+        </p>
+        <p>
+          <strong>Address:</strong> {userData.address}
+        </p>
+      </div>
     </div>
   );
 };
